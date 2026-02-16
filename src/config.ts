0404a767917e5b81beb55970e19e42179c4e4106
Abhrajit Ghosh <157137147+u4ia-abhra@users.noreply.github.com>
2026-02-16 16:04:43 +0530

    Update site configuration for new user details

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Abhrajit Ghosh",
+  title: "Software Engineer",
+  description: "Portfolio website of Abhrajit Ghosh",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
-    twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    email: "abhrajitgh@gmail.com",
+    linkedin: "https://www.linkedin.com/in/abhrajitghosh/",
+    // twitter: "https://x.com/rfitzio",
+    github: "https://github.com/u4ia-abhra",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
